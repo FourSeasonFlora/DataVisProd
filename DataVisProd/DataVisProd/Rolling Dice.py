5e25560672d9c75360cfa884db8b459f53c6ff06
FourSeasonFlora <fourseasonflora@gmail.com>
2022-02-03 17:39:15 -0500

    pre vis

---
+++ DataVisProd/DataVisProd/Rolling Dice.py
@@ -0,0 +1,21 @@
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+from die import Die
+
+# creating a D6. 
+die = Die()
+
+# Make some rolls and store results in a list. 
+results = []
+for roll_num in range(1000):
+    result = die.roll()
+    results.append(result)
+
+# Analyze the results.
+frequencies = []
+for value in range(1, die.num_sides +1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print (frequencies)
+
